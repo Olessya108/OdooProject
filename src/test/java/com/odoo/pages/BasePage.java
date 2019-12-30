674ebae892f6dae738410ca0f0f0d851f9acca14
Olessya Foxborn <olessyafoxborn@gmail.com>
2019-12-30 17:59:02 -0500

    first

--- src/test/java/com/odoo/pages/BasePage.java
+++ src/test/java/com/odoo/pages/BasePage.java
@@ -3,5 +3,7 @@ package com.odoo.pages;
 public abstract class BasePage {
     public static void main(String[] args) {
         System.out.println("Olessya");
+        System.out.println("Olessya");
+
     }
 }
